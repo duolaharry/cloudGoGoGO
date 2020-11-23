09afcab2803515b59971b163a9de8691dc9cab40
duolaharry <1041219412@qq.com>
2020-11-23 22:14:44 +0800

    优化了输出接口

--- compute_part/long_time_streaming.py
+++ compute_part/long_time_streaming.py
@@ -19,7 +19,6 @@ def mapByName(x):
 
 
 if __name__ == "__main__":
-
     # 设置流监听，监听源为hdfs文件系统
     sc = SparkContext(appName="PythonStreamingLongTime")
     ssc = StreamingContext(sc, 30)
@@ -27,6 +26,7 @@ if __name__ == "__main__":
     douyuPopu = ssc.textFileStream("hdfs://wcy-pc:9000/longtime/douyu/popular/")
     huyaPeople = ssc.textFileStream("hdfs://wcy-pc:9000/longtime/huya/people/")
     huyaPopu = ssc.textFileStream("hdfs://wcy-pc:9000/longtime/huya/popular/")
+    data = []  # 保存输出流
 
     # 对直播人数rdd进行处理
     douyuPeopleCounts = douyuPeople.map(lambda x: (mapBynum(x)[0], mapBynum(x)[1])) \
@@ -34,17 +34,22 @@ if __name__ == "__main__":
     huyaPeopleCounts = huyaPeople.map(lambda x: (mapBynum(x)[0], mapBynum(x)[1])) \
         .reduceByKey(lambda a, b: a + b)
 
+
     def outputDouyuPeopleCounts(time, rdd):
         s = "Time: %s" % time
-        # print("-------------------------------------------")
+        print("-------------------------------------------")
         # print("outputpopuCounts Time: %s" % time)
         # print("-------------------------------------------")
         taken = rdd.take(1)
         if len(taken) > 0:
             record = taken[0]
             print(["斗鱼", "people", record[1]])
+            item = "斗鱼 " + "people " + str(record[1])
+            data.append(item)
+
 
     douyuPeopleCounts.repartition(1).foreachRDD(outputDouyuPeopleCounts)
+
 
     def outputHuyaPeopleCounts(time, rdd):
         s = "Time: %s" % time
@@ -55,6 +60,9 @@ if __name__ == "__main__":
         if len(taken) > 0:
             record = taken[0]
             print(["虎牙", "people", record[1]])
+            item = "虎牙 " + "people " + str(record[1])
+            data.append(item)
+
 
     huyaPeopleCounts.repartition(1).foreachRDD(outputHuyaPeopleCounts)
 
@@ -63,6 +71,7 @@ if __name__ == "__main__":
         .reduceByKey(lambda a, b: a + b)
     huyaPopuCounts = huyaPopu.map(lambda x: (mapBynum(x)[0], mapBynum(x)[1])) \
         .reduceByKey(lambda a, b: a + b)
+
 
     def outputDouyuPopuCounts(time, rdd):
         s = "Time: %s" % time
@@ -73,8 +82,12 @@ if __name__ == "__main__":
         if len(taken) > 0:
             record = taken[0]
             print(["斗鱼", "popu", record[1]])
+            item = "斗鱼 " + "popu " + str(record[1])
+            data.append(item)
+
 
     douyuPopuCounts.repartition(1).foreachRDD(outputDouyuPopuCounts)
+
 
     def outputHuyaPopuCounts(time, rdd):
         s = "Time: %s" % time
@@ -85,6 +98,9 @@ if __name__ == "__main__":
         if len(taken) > 0:
             record = taken[0]
             print(["虎牙", "popu", record[1]])
+            item = "虎牙 " + "popu " + str(record[1])
+            data.append(item)
+
 
     huyaPopuCounts.repartition(1).foreachRDD(outputHuyaPopuCounts)
 
@@ -93,6 +109,7 @@ if __name__ == "__main__":
         .reduceByKey(lambda a, b: a + b)
     huyaPeopleCountsForeach = huyaPeople.map(lambda x: (mapByName(x)[0], mapByName(x)[1])) \
         .reduceByKey(lambda a, b: a + b)
+
 
     def outputDouyuPeopleCountsForeach(time, rdd):
         s = "Time: %s" % time
@@ -103,8 +120,12 @@ if __name__ == "__main__":
         if len(taken) > 0:
             for record in taken[:20]:
                 print(["斗鱼", "people", record[0], record[1]])
+                item = "斗鱼 " + "people " + record[0] + " " + str(record[1])
+                data.append(item)
+
 
     douyuPeopleCountsForeach.repartition(1).foreachRDD(outputDouyuPeopleCountsForeach)
+
 
     def outputHuyaPeopleCountsForeach(time, rdd):
         s = "Time: %s" % time
@@ -115,6 +136,9 @@ if __name__ == "__main__":
         if len(taken) > 0:
             for record in taken[:20]:
                 print(["虎牙", "people", record[0], record[1]])
+                item = "虎牙 " + "people " + record[0] + " " + str(record[1])
+                data.append(item)
+
 
     huyaPeopleCountsForeach.repartition(1).foreachRDD(outputHuyaPeopleCountsForeach)
 
@@ -123,6 +147,7 @@ if __name__ == "__main__":
         .reduceByKey(lambda a, b: a + b)
     huyaPopuCountsForeach = huyaPopu.map(lambda x: (mapByName(x)[0], mapByName(x)[1])) \
         .reduceByKey(lambda a, b: a + b)
+
 
     def outputDouyuPopuCountsForeach(time, rdd):
         s = "Time: %s" % time
@@ -133,8 +158,12 @@ if __name__ == "__main__":
         if len(taken) > 0:
             for record in taken[:20]:
                 print(["斗鱼", "popu", record[0], record[1]])
+                item = "斗鱼 " + "popu " + record[0] + " " + str(record[1])
+                data.append(item)
+
 
     douyuPopuCountsForeach.repartition(1).foreachRDD(outputDouyuPopuCountsForeach)
+
 
     def outputHuyaPopuCountsForeach(time, rdd):
         s = "Time: %s" % time
@@ -143,13 +172,19 @@ if __name__ == "__main__":
         # print("-------------------------------------------")
         taken = rdd.take(21)
         if len(taken) > 0:
+
             for record in taken[:20]:
                 print(["虎牙", "popu", record[0], record[1]])
-            print()
+                item = "虎牙 " + "popu " + record[0] + " " + str(record[1])
+                data.append(item)
+            file = open("/home/pluviophile/Documents/tmp/longtime/output1/output.txt", "w", encoding='utf-8')
+            for i in data:
+                file.write(i + "\n")
+            file.close()
+            data.clear()
 
     huyaPopuCountsForeach.repartition(1).foreachRDD(outputHuyaPopuCountsForeach)
 
     # 开始监听
     ssc.start()
     ssc.awaitTermination()
-
