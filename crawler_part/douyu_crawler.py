d1b2d31f871478b00493b054d28b942407f2cdd7
NJUSE18jyx <181250059@smail.nju.edu.cn>
2020-11-25 17:04:30 +0800

    second commit

--- crawler_part/douyu_crawler.py
+++ crawler_part/douyu_crawler.py
@@ -1,5 +1,4 @@
 import requests
-import time
 
 
 class DouYu(object):
@@ -8,15 +7,12 @@ class DouYu(object):
         self.url = 'https://www.douyu.com/gapi/rkc/directory/0_0/'
         self.page = 1
         self.num = 1
-        self.artist_info = {}
-        self.total_hot = 0
         self.flag = 1
 
     def __get_response(self):  # 请求接口，返回响应
         res = requests.get(self.url+str(self.page))
         response = res.json()
         return response
-
 
     def __get_first_name(self):  # 拿到第一页第一个的主播姓名，用来判断循环重复
         url = 'https://www.douyu.com/gapi/rkc/directory/0_0/1'
@@ -25,8 +21,10 @@ class DouYu(object):
         first_name = response['data']['rl'][0]['nn']
         return first_name
 
-    def get_artist_info(self):
+    def crawl(self):
         first_name = self.__get_first_name()  # 得到当前排名第一个主播姓名，用来判断循环结束
+        file_people = open('./Long/douyu/people.txt', 'w')
+        file_popular = open('./Long/douyu/popular.txt', 'w')
 
         while DouYu.flag == 1:
             response = self.__get_response()
@@ -40,50 +38,16 @@ class DouYu(object):
                     if first_name == name:
                         self.flag = 0
                         break
-                game_and_hot = []
-                game_and_hot.append(game)
-                game_and_hot.append(hot)
-                self.artist_info[name] = game_and_hot
                 self.num += 1  # 计算直播数量
                 print("{}.主播姓名:{}, 人气:{:,},直播类型:{}".format(self.num, name, hot, game))
+                file_people.write('name:{} num:1\n'.format(game))
+                file_popular.write('name:{} num:{}\n'.format(game, hot))
             DouYu.page += 1
-        return self.artist_info
-
-    def analyze_info(self, artist_info):
-        game_num = {}
-        info = sorted(artist_info.items(), key=lambda x: x[1][1], reverse=True)
-        print("\n==统计时间为:{}".format(time.strftime("%Y-%m-%d %X", time.localtime())))
-        game_list = {}
-        for l in info:
-            game = l[1][0]
-            hot = l[1][1]
-            self.total_hot += hot  # 统计总热度
-            game_list[game] = game_list.get(game, 0) + hot
-            game_num[game] = game_num.get(game, 0) + 1
-        game_num = sorted(game_num.items(), key=lambda x: x[1], reverse=True)
-        game_list = sorted(game_list.items(), key=lambda x: x[1], reverse=True)
-
-        # 分类热度
-        file = open('./Long/douyu/popular.txt', 'w')
-        print("当前斗鱼直播类型热度排名前十:")
-        for k in range(10):
-            game_name = game_list[k][0]
-            game_hot = game_list[k][1]
-            print("name:{} num:{}".format(game_name, game_hot))
-            file.write("name:{} num:{}\n".format(game_name, game_hot))
-        file.close()
-
-        # 直播人数
-        file = open('./Long/douyu/people.txt', 'w')
-        print("\n当前斗鱼最多人在直播的类型前十:")
-        for p in range(10):
-            game_name = game_num[p][0]
-            game_number = game_num[p][1]
-            print("name:{} num:{}".format(game_name, game_number))
-            file.write("name:{} num:{}\n".format(game_name, game_number))
-        file.close()
+            print('第{}页完结'.format(DouYu.page))
+        file_people.close()
+        file_popular.close()
 
 
 if __name__ == '__main__':
     DouYu = DouYu()
-    DouYu.analyze_info(DouYu.get_artist_info())
+    DouYu.crawl()
--- crawler_part/huya_crawler.py
+++ crawler_part/huya_crawler.py
@@ -1,9 +1,11 @@
 import requests
-import time
 import json
 
-page, count, num, lis, game_list, hot_list = 1, 0, 0, {}, {}, {}
+file_people = open('./Long/huya/people.txt', 'w')
+file_popular = open('./Long/huya/popular.txt', 'w')
+page, count, num = 1, 0, 0
 url = "https://www.huya.com/cache.php?m=LiveList&do=getLiveListByPage&tagAll=0&page="
+
 while True:
     res = requests.get(url+str(page))
     response = res.json()
@@ -14,45 +16,19 @@ while True:
 
     # 如果响应为空的话，代表遍历结束，数据分析打印
     if artists is None or artists == []:
-        lis = sorted(lis.items(), key=lambda x: x[1][0], reverse=True)
-
-        # 直播人数
-        file = open('./Long/huya/people.txt', 'w')
-        game_list = sorted(game_list.items(), key=lambda x: x[1], reverse=True)
-        hot_list = sorted(hot_list.items(), key=lambda x: x[1], reverse=True)
-        count = 0
-        for game in game_list:
-            if count >= 10:
-                break
-            print("name:{} num:{}".format(game[0], game[1]))
-            file.write("name:{} num:{}\n".format(game[0], game[1]))
-            count += 1
-        file.close()
-        # ----
-
-        # 分类热度
-        file = open('./Long/huya/popular.txt', 'w')
-        print("\n当前直播类型人气排名:")
-        for hot in range(10):
-            print("name:{} num:{}".format(hot_list[hot][0], hot_list[hot][1]))
-            file.write("name:{} num:{}\n".format(hot_list[hot][0], hot_list[hot][1]))
-        file.close()
-        # ----
         break
 
     for i in artists:
-        store = []
         nick = i['nick']
-        introduction = i['introduction']
         totalCount = int(i['totalCount'])  # 人气
         gamename = i['gameFullName']
         count += int(totalCount)
         num += 1
         print("{}.主播姓名:{}人气:{:,}".format(num, nick, int(totalCount)))
-        store.append(totalCount)
-        store.append(gamename)
-        lis[nick] = store
-        game_list[gamename] = game_list.get(gamename, 0) + 1
-        hot_list[gamename] = hot_list.get(gamename, 0) + totalCount
+        file_people.write('name:{} num:1\n'.format(gamename))
+        file_popular.write('name:{} num:{}\n'.format(gamename, totalCount))
     print("第%s页结束" % page)
     page += 1
+
+file_people.close()
+file_popular.close()
