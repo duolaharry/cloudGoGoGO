129f62bf3b956255b7943e7bfddb36e9714d734b
DDCHlsq <iamddch@gmail.com>
2020-11-27 00:26:04 +0800

    mission success

--- graph_part/long/test.sh
+++ graph_part/long/test.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-# for ((COUNT = 1; COUNT <= 4; COUNT++)); do
-#   cat ./test.txt | head -n $COUNT | tail -n 1
-#   sleep 1
-# done
 while :
 do
-    cat ./test.txt ; echo
-    sleep 25
+    cat ./test.txt
+    sleep 90
 done
--- graph_part/short/test.sh
+++ graph_part/short/test.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-cat ./test.txt ; echo
+while :
+do
+    cat ./test.txt
+    sleep 90
+done
